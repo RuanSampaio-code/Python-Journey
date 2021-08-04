b7a8ccbe25beca154c00cb2b3acd361d992c32ea
RuanSampaio-code <ruansampaio001@gmail.com>
2021-08-04 09:24:29 -0300

    ad

---
+++ Programas basicos/ex010.py
@@ -0,0 +1,2 @@
+n = int(input('Digite um número para ver sua tabuada: '))
+print('{}X{}== {}'.format(1,n,n*1 ))
---
+++ Programas basicos/ex011.py
@@ -0,0 +1,8 @@
+n1 = float(input(" digite uma altura: "))
+n2 = float(input(" digite uma largura: "))
+a = n1 * n2
+t = a / 2
+print(" O total da área é {}M². \n "
+      "Então a quantidade de tinta que terá que ser usado será {}L \n "
+      "Considerando que a cada 1 litro de tinta pinta 2M² quadrados"
+      " \n ....Fim do sístema.... ".format(a, t))
