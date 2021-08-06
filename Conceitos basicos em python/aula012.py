116d5a627fb9aca5744e2f58c8aa56534710a3ed
RuanSampaio-code <ruansampaio001@gmail.com>
2021-08-06 10:20:46 -0300

    Create aula012.py

---
+++ Conceitos basicos em python/aula012.py
@@ -0,0 +1,7 @@
+print('====Estrutura de repetição FOR====')
+print(' O range  um tipo de sequência imutável, com inicio, fim e passo, respectivamente  ')
+for x in range(1,6):
+    print(x)
+print('====Estrutura de repetição FOR COM IMCREMENTO ====')
+for x in range(1,6,2):
+    print(x)
