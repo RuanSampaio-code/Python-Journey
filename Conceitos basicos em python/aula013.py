d9d94dd189d00a82961a24dc87c55cc82f386cec
RuanSampaio-code <ruansampaio001@gmail.com>
2021-08-07 07:35:31 -0300

    Update aula013.py

--- Conceitos basicos em python/aula013.py
+++ Conceitos basicos em python/aula013.py
@@ -4,4 +4,11 @@ o while é usadno quando não se sabe o limmite de repetição:
 é uma estrutura de repetição com teste logico.
 Segue o exemplo:
 ''')
-while
+print('Exemplo com while da msm situação anterior:')
+x=0
+while x<6:
+    x=x+1
+    print(x)
+print('fim')
+    
+
