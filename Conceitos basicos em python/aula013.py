97ad6b349aa892685b3e493589499fc009789342
RuanSampaio-code <ruansampaio001@gmail.com>
2021-08-07 06:59:50 -0300

    Create aula013.py

---
+++ Conceitos basicos em python/aula013.py
@@ -0,0 +1,7 @@
+print('ESTRUTURA DE REPETIÇÃO WHILE')
+print('''Situações que aparece deve aparecer o while:
+o while é usadno quando não se sabe o limmite de repetição:
+é uma estrutura de repetição com teste logico.
+Segue o exemplo:
+''')
+while
